a830c96d5220785a7a5647deda51a1cbdea0f6a2
Gustavo Sorati <sorati.gustavo@gmail.com>
2022-07-26 20:23:49 -0300

    correção /post api prisma <unknown> to <any>

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -84,8 +84,9 @@ export default NextAuth({
 
         console.log(x)
         return true;
-      } catch {
-      return false
+      } catch (err) {
+        console.log(err)
+        return false
       }
     }
   },
